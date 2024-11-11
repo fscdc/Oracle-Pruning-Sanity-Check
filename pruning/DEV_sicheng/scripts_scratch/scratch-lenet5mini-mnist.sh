758dd60c9c6c748f597706f1d5dae5b254d98f50
冯思程 <fscdyx888@163.com>
2024-11-11 23:21:29 +0800

    webpage init

--- pruning/DEV_sicheng/scripts_scratch/scratch-lenet5mini-mnist.sh
+++
@@ -1,8 +0,0 @@
-export NCCL_P2P_DISABLE="1"
-export NCCL_IB_DISABLE="1"
-
-CUDA_VISIBLE_DEVICES=1 python main.py --no_ddp --no_scp \
-    -a lenet5_mini --dataset mnist \
-    --lr 0:0.01,20:0.001 --epochs 30 \
-    --weight_decay 1e-4 \
-    --experiment_name scratch__lenet5_mini__mnist__epochs30_lr0.01to0.001
